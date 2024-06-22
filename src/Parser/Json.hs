f4abf04e1da3bf6e345178b8897fd1441cdbb4c5
nkarl <21993921+nkarl@users.noreply.github.com>
2024-06-22 10:45:52 -0700

    Added a test case for Json.

--- src/Parser/Json.hs
+++ src/Parser/Json.hs
@@ -120,26 +120,32 @@ test = do
             )
         , TestCase
             ( let expected = Nothing
-                  actual = (unwrap $ parseChar 'n') "aaa"
-                  title = "parse one char `n` from the string `aaa`, producing Nothing"
+                  actual = (unwrap $ parseChar 'x') "aaa"
+                  title = "parse one char `x` not in the string `aaa`, producing Nothing"
                in assertEqual title expected actual
             )
         , TestCase
             ( let expected = Just ("abcde", "abcde")
                   actual = unwrap parseString "abcde"
-                  title = "parse a string continuously"
+                  title = "parse en entire string, keeping the original for comparison"
                in assertEqual title expected actual
             )
         , TestCase
             ( let expected = Just ("defgh", "abc")
                   actual = (unwrap . parseString1) "abc" "abcdefgh"
-                  title = "parse a string continuously"
+                  title = "parse a substring `abc` from a string `abcdefgh"
                in assertEqual title expected actual
             )
         , TestCase
             ( let expected = Just ("defgh", "abc")
                   actual = (unwrap . parseString2) "abc" "abcdefgh"
-                  title = "parse a string continuously"
+                  title = "parse a substring `abc` from a string `abcdefgh`"
+               in assertEqual title expected actual
+            )
+        , TestCase
+            ( let expected = Nothing
+                  actual = (unwrap . parseString1) "xbc" "abcdefgh"
+                  title = "parse a substring `xbc` not in a string `abcdefgh`, producing Nothing"
                in assertEqual title expected actual
             )
         ]
